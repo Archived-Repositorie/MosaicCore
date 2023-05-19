2b15d620067bbe86523e76fc45a69105f63f7acc
JustFoxx <justfoxx@outlook.com>
2023-05-19 22:17:06 +0200

    Forgot to remove testing functions

--- src/main/kotlin/io/github/mosaicmc/mosaiccore/Main.kt
+++ src/main/kotlin/io/github/mosaicmc/mosaiccore/Main.kt
@@ -3,9 +3,7 @@
 
 package io.github.mosaicmc.mosaiccore
 
-import io.github.mosaicmc.mosaiccore.event.*
 import io.github.mosaicmc.mosaiccore.plugin.BeforePluginInitializer
-import io.github.mosaicmc.mosaiccore.plugin.PluginContainer
 import io.github.mosaicmc.mosaiccore.plugin.PluginInitializer
 import net.fabricmc.loader.api.FabricLoader
 import org.slf4j.LoggerFactory
@@ -17,29 +15,7 @@ internal val beforePlugins = FabricLoader.getInstance().getEntrypointContainers(
 
 fun preInit() {
     logger.info("Welcome to mosaicmc!")
-    EventHandler.registerEvent(TestEvent::class)
 }
-
-fun test(plugin: PluginContainer) {
-    listener(plugin) {
-        subscriber(TestEvent::class) {
-            plugin.logger.info("Test event called! 0")
-        }
-        subscriber(TestEvent::class) {
-            plugin.logger.info("Test event called! 1")
-        }
-        subscriber(TestEvent::class, SubscriberData(Priority.HIGHEST)) {
-            plugin.logger.info("Test event called! 2")
-        }
-        subscriber(TestEvent::class) {
-            plugin.logger.info("Test event called! 3")
-        }
-    }.register()
-    EventHandler.callEvent(TestEvent())
-}
-
-
-class TestEvent : Event
 
 
 
