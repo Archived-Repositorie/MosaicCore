de8d989d22e7972cdb59c77a238bfd2c0d37a8f6
JustFoxx <justfoxx@outlook.com>
2023-05-24 21:53:11 +0200

    Ported mixins over to kotlin

---
+++ src/main/kotlin/io/github/mosaicmc/mosaiccore/mixins/MinecraftServerMixin.kt
@@ -0,0 +1,38 @@
+package io.github.mosaicmc.mosaiccore.mixins
+
+import io.github.mosaicmc.mosaiccore.beforePlugins
+import io.github.mosaicmc.mosaiccore.plugin.PluginContainer
+import io.github.mosaicmc.mosaiccore.plugins
+import net.minecraft.server.MinecraftServer
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
+
+@Mixin(MinecraftServer::class)
+class MinecraftServerMixin {
+    @Inject(
+        at = [At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;setupServer()Z")],
+        method = ["runServer"]
+    )
+    private fun pluginLoader(info: CallbackInfo) {
+        val beforePlugins = beforePlugins
+        for (plugin in beforePlugins) {
+            val entryPoint = plugin.entrypoint
+            entryPoint.beforeLoad()
+        }
+        val plugins = plugins
+        for (plugin in plugins) {
+            val entryPoint = plugin.entrypoint
+            val modContainer = plugin.provider
+            val server = this as MinecraftServer
+            entryPoint.onLoad(
+                PluginContainer(
+                    modContainer,
+                    server,
+                )
+            )
+        }
+    }
+}
