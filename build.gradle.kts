20ec35b58e36cafc110cecff3d03c1cb771b697a
justfoxx <justfoxxo@outlook.com>
2023-07-17 08:47:04 +0200

    gradle clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fabric-loom") version "1.3.8"
+    id("fabric-loom") version "1.3-SNAPSHOT"
     id("maven-publish")
     id("org.jetbrains.kotlin.jvm")
     id("com.ncorti.ktfmt.gradle") version "0.12.0"
@@ -10,7 +10,7 @@ loom {
 }
 
 val sourceCompatibility = JavaVersion.VERSION_17
-val targetCompatibility = sourceCompatibility
+val targetCompatibility = JavaVersion.VERSION_17
 val archivesBaseName = project.properties["archivesBaseName"].toString()
 
 version = project.properties["mod_version"].toString()
@@ -22,9 +22,9 @@ repositories {
 
 dependencies {
     minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
-    mappings("net.fabricmc:yarn:${project.properties["minecraft_version"]}+${project.properties["yarn_mappings"]}:v2")
+    mappings("net.fabricmc:yarn:${project.properties["minecraft_version"]}+build.${project.properties["yarn_mappings"]}:v2")
     modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}.${project.properties["kotlin_version"]}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}+kotlin.${project.properties["kotlin_version"]}")
     testImplementation("net.fabricmc:fabric-loader-junit:${project.properties["loader_version"]}")
 
     val mixinExtras = "com.github.LlamaLad7:MixinExtras:${project.properties["mixin_extras"]}"
@@ -51,7 +51,7 @@ tasks {
     }
 
     withType<JavaCompile>().configureEach {
-        options.release.set(17)
+        options.release = 17
     }
 }
 
